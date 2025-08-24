c8474a2807067e2acaaff86b54712b03ee8e8c40
suman112233773 <bunnyboyx15@gmail.com>
2025-08-24 12:34:34 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~MrFBHBiJ#BRvmPasnm7yHTZ2hhsnReIf45sOzQEwmfgI2upQzj_s",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "917679396342", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
